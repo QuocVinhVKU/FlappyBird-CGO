39a45a28412028077ce21fceac081f47ebca1d54
Quoc-Vinh Dang <80034287+QuocVinhVKU@users.noreply.github.com>
2023-06-25 15:16:03 +0700

    Update ColliderManager.cs

--- Assets/Scripts/ColliderManager.cs
+++ Assets/Scripts/ColliderManager.cs
@@ -4,18 +4,7 @@ using UnityEngine;
 
 public class ColliderManager : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D birdBody;
 
-    void Start()
-    {
-        birdBody = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.name == "Ground" || (collision.gameObject.name == "OngCong"))
